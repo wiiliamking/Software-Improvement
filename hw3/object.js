2a8da7a761e9bb94abd694a0ab334d524884a9d6
williamking <williamjwking@gmail.com>
2015-03-18 21:19:22 +0800

    Complete part 1

--- hw3/object.js
+++ hw3/object.js
@@ -1,7 +1,8 @@
 function Base(variable) {
 	this.instanceVariable = variable;
 	this.instanceMethod = function() {
-		console.log("This is from Base class instance-method, static-variable is: " + this.instanceVariable);
+		// this.staticMethod();
+		console.log("This is from Base class instance-method, instance-variable is: " + this.instanceVariable);
 	}
 }
 
@@ -9,37 +10,40 @@ Base.prototype = {
 	constructor: Base,
 	staticVariable: 'Base',
 	staticMethod: function() {
-		console.log("This is from Base class instance-method, instance-variable is: " + this.staticVariable);
+		console.log("This is from Base class static-method, static-variable is: " + this.staticVariable);
 	}
 }
 
-function extend(base, derived) {
-	derived = function(variable) {
-		base.call(this, variable);
+function extend(Base, Derived) {
+	Derived = function(variable) {
+		this.instanceVariable = variable;
 		this.instanceMethod = function() {
-			base.instanceMethod();
+			this.staticMethod();
+			var baseInstanceMethod = Derived.prototype.instanceMethod;
+			baseInstanceMethod.call(this);
 			console.log("This is from Derived class instance-method, instance-variable is: " + this.instanceVariable);
 		}
 	}
-	derived.prototype = {
-		constructor: derived,
-		staticVariable: 'Derived',
-		staticMethod: function() {
-			base.staticMethod();
-			console.log("This is from Derived class static-method, static-variable is: " + this.staticVariable);
-		}
-	}
-	return derived;
+    Derived.prototype = new Base();
+    Derived.prototype.staticMethod = function() {
+    	var baseStaticMethod = Derived.prototype.__proto__.staticMethod;
+    	baseStaticMethod.call(this);
+    	console.log("This is from Derived class static-method, static-variable is: " + this.staticVariable);
+    }
+    Derived.prototype.staticVariable = 'Derived';
+	Derived.__proto__ = Derived.prototype;
+	return Derived;
 }
 
-var object = new Base("hehe");
-Derived = undefined;
+var Derived = undefined;
 Derived = extend(Base, Derived);
-
-console.log(Base);
-
 var example = new Derived("example");
 Derived.staticMethod();
 example.instanceMethod();
 
+console.log("\m");
 
+example = new Derived('example');
+otherExample = new Derived('other-example');
+example.instanceMethod();
+otherExample.instanceMethod();
