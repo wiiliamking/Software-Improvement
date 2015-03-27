15de69b12712d9770c888fbb237f5a60522b38a4
williamking <williamjwking@gmail.com>
2015-03-27 12:02:57 +0800

    ..

--- hw4/S5/index.js
+++ hw4/S5/index.js
@@ -1,3 +1,21 @@
+var Promise = function() {
+    var success = null;
+}
+
+Promise.prototype.then = function(successHandler, failhandler) {
+    if (success == null) return;
+    if (success) successHandler();
+    else failhandler();
+}
+
+Promise.prototype.resovle = function() {
+    success = 1;
+}
+
+Promise.prototype.reject = function() {
+    success = 0;
+}
+
 function init() {
 	var buttons = document.getElementsByClassName("button");
     for (var i = 0; i < buttons.length; ++i) {
@@ -6,55 +24,227 @@ function init() {
         classList.remove("unable");
         buttons[i].getElementsByClassName("unread")[0].classList.remove("show");
         buttons[i].getElementsByClassName("unread")[0].classList.add("hide");
+        buttons[i].getElementsByClassName("message")[0].classList.remove("show");
+        buttons[i].getElementsByClassName("message")[0].classList.add("hide");
     }
     document.getElementById("info-bar").classList.remove("clickable");
     document.getElementById("sum").classList.add("disappear");
+    document.getElementById("lastMessage").classList.add("disappear");
     document.getElementById("info-bar").removeEventListener("click", showTheSum);
-    var sum = adder(0);
-    adder(sum * -1);
 }
 
 function react(e) {
     getNumber(e.currentTarget);
 }
 
-function aHandler(message, currentSum, func) {
-    var result = [];
-    vat err = Math.round(Math.random());
-    if (err) {
-        func()
-    }
-    if (err) return result;
+function aHandler(message, currentSum, errHandler) {
     var button = document.getElementsByClassName("button")[0];
     var xmlHttp = new XMLHttpRequest();
     if (xmlHttp == null) alert('XMLHTTP not supported.');
-    xmlHttp.onreadystatechange = function (button) {
+    var promise = new Promise();
+    xmlHttp.onreadystatechange = function (button, errHandler) {
         return function() {
             if (xmlHttp.readyState == 4) {
                 if (xmlHttp.status == 200) {
-                    button.getElementsByClassName("unread")[0].innerHTML = xmlHttp.responseText;
-                    adder(parseInt(xmlHttp.responseText));
                     button.classList.add("completed");
-                    recoverButtons();
-                    if (document.getElementsByClassName("completed").length < 5) nextButton();
+                    var result = [];
+                    var err = Math.round(Math.random());
+                    if (err)  {
+                        errHandler("这不是个天大的秘密", currentSum, button);
+                    } else {
+                        currentSum += parseInt(xmlHttp.responseText);
+                        button.getElementsByClassName("unread")[0].innerHTML = xmlHttp.responseText;
+                        button.getElementsByClassName("message")[0].innerHTML = message;
+                    }
+                    button.getElementsByClassName("message")[0].classList.remove("hide");
+                    button.getElementsByClassName("message")[0].classList.add("show");
+                    bHandler("我不知道", currentSum, function(message, currentSum, button) {
+                        button.getElementsByClassName("message")[0].innerHTML = message;
+                        button.getElementsByClassName("unread")[0].innerHTML = "X";
+                    })
                 }
-                if (document.getElementsByClassName("completed").length == 5) {
-                    document.getElementById("sum").innerHTML = currentSum;
-                    showTheSum();
-                }
+                // if (document.getElementsByClassName("completed").length == 5) {
+                //     document.getElementById("sum").innerHTML = currentSum;
+                //     showTheSum();
+                // }
             }
         };
-    }(button);
+    }(button, errHandler);
     var span = button.getElementsByClassName("unread")[0];
     span.innerHTML = "...";
     span.classList.remove("hide");
     span.classList.add("show");
     xmlHttp.open("GET", "/", true);
-    button.removeEventListener("click", getNumber);
-    closeButtons(button);
     xmlHttp.send(null);
-} 
+}
 
+function bHandler(message, currentSum, errHandler) {
+    var button = document.getElementsByClassName("button")[1];
+    var xmlHttp = new XMLHttpRequest();
+    if (xmlHttp == null) alert('XMLHTTP not supported.');
+    var promise = new Promise();
+    xmlHttp.onreadystatechange = function (button, errHandler) {
+        return function() {
+            if (xmlHttp.readyState == 4) {
+                if (xmlHttp.status == 200) {
+                    button.classList.add("completed");
+                    var result = [];
+                    var err = Math.round(Math.random());
+                    if (err)  {
+                        errHandler("我知道", currentSum, button);
+                    } else {
+                        currentSum += parseInt(xmlHttp.responseText);
+                        button.getElementsByClassName("unread")[0].innerHTML = xmlHttp.responseText;
+                        button.getElementsByClassName("message")[0].innerHTML = message;
+                    }
+                    button.getElementsByClassName("message")[0].classList.remove("hide");
+                    button.getElementsByClassName("message")[0].classList.add("show");
+                    cHandler("你不知道", currentSum, function(message, currentSum, button) {
+                        button.getElementsByClassName("message")[0].innerHTML = message;
+                        button.getElementsByClassName("unread")[0].innerHTML = "X";
+                    });
+                }
+                // if (document.getElementsByClassName("completed").length == 5) {
+                //     document.getElementById("sum").innerHTML = currentSum;
+                //     showTheSum();
+                // }
+            }
+        };
+    }(button, errHandler);
+    var span = button.getElementsByClassName("unread")[0];
+    span.innerHTML = "...";
+    span.classList.remove("hide");
+    span.classList.add("show");
+    xmlHttp.open("GET", "/", true);
+    xmlHttp.send(null);
+}
+
+function cHandler(message, currentSum, errHandler) {
+    var button = document.getElementsByClassName("button")[2];
+    var xmlHttp = new XMLHttpRequest();
+    if (xmlHttp == null) alert('XMLHTTP not supported.');
+    var promise = new Promise();
+    xmlHttp.onreadystatechange = function (button, errHandler) {
+        return function() {
+            if (xmlHttp.readyState == 4) {
+                if (xmlHttp.status == 200) {
+                    button.classList.add("completed");
+                    var result = [];
+                    var err = Math.round(Math.random());
+                    if (err)  {
+                        errHandler("你知道", currentSum, button);
+                    } else {
+                        currentSum += parseInt(xmlHttp.responseText);
+                        button.getElementsByClassName("unread")[0].innerHTML = xmlHttp.responseText;
+                        button.getElementsByClassName("message")[0].innerHTML = message;
+                    }
+                    button.getElementsByClassName("message")[0].classList.remove("hide");
+                    button.getElementsByClassName("message")[0].classList.add("show");
+                    dHandler("他不知道", currentSum, function(message, currentSum, button) {
+                        button.getElementsByClassName("message")[0].innerHTML = message;
+                        button.getElementsByClassName("unread")[0].innerHTML = "X";
+                    });
+                }
+                // if (document.getElementsByClassName("completed").length == 5) {
+                //     document.getElementById("sum").innerHTML = currentSum;
+                //     showTheSum();
+                // }
+            }
+        };
+    }(button, errHandler);
+    var span = button.getElementsByClassName("unread")[0];
+    span.innerHTML = "...";
+    span.classList.remove("hide");
+    span.classList.add("show");
+    xmlHttp.open("GET", "/", true);
+    xmlHttp.send(null);
+}
+
+function dHandler(message, currentSum, errHandler) {
+    var button = document.getElementsByClassName("button")[3];
+    var xmlHttp = new XMLHttpRequest();
+    if (xmlHttp == null) alert('XMLHTTP not supported.');
+    var promise = new Promise();
+    xmlHttp.onreadystatechange = function (button, errHandler) {
+        return function() {
+            if (xmlHttp.readyState == 4) {
+                if (xmlHttp.status == 200) {
+                    button.classList.add("completed");
+                    var result = [];
+                    var err = Math.round(Math.random());
+                    if (err)  {
+                        errHandler("他知道", currentSum, button);
+                    } else {
+                        currentSum += parseInt(xmlHttp.responseText);
+                        button.getElementsByClassName("unread")[0].innerHTML = xmlHttp.responseText;
+                        button.getElementsByClassName("message")[0].innerHTML = message;
+                    }
+                    button.getElementsByClassName("message")[0].classList.remove("hide");
+                    button.getElementsByClassName("message")[0].classList.add("show");
+                    eHandler("才怪", currentSum, function(message, currentSum, button) {
+                        button.getElementsByClassName("message")[0].innerHTML = message;
+                        button.getElementsByClassName("unread")[0].innerHTML = "X";
+                    });
+                }
+                // if (document.getElementsByClassName("completed").length == 5) {
+                //     document.getElementById("sum").innerHTML = currentSum;
+                //     showTheSum();
+                // }
+            }
+        };
+    }(button, errHandler);
+    var span = button.getElementsByClassName("unread")[0];
+    span.innerHTML = "...";
+    span.classList.remove("hide");
+    span.classList.add("show");
+    xmlHttp.open("GET", "/", true);
+    xmlHttp.send(null);
+}
+
+function eHandler(message, currentSum, errHandler) {
+    var button = document.getElementsByClassName("button")[4];
+    var xmlHttp = new XMLHttpRequest();
+    if (xmlHttp == null) alert('XMLHTTP not supported.');
+    var promise = new Promise();
+    xmlHttp.onreadystatechange = function (button, errHandler) {
+        return function() {
+            if (xmlHttp.readyState == 4) {
+                if (xmlHttp.status == 200) {
+                    button.classList.add("completed");
+                    var result = [];
+                    var err = Math.round(Math.random());
+                    if (err)  {
+                        errHandler("才怪...个锤子", currentSum, button);
+                    } else {
+                        currentSum += parseInt(xmlHttp.responseText);
+                        button.getElementsByClassName("unread")[0].innerHTML = xmlHttp.responseText;
+                        button.getElementsByClassName("message")[0].innerHTML = message;
+                    }
+                    button.getElementsByClassName("message")[0].classList.remove("hide");
+                    button.getElementsByClassName("message")[0].classList.add("show");
+                    bubbleHandler("才怪", currentSum);
+                }
+                // if (document.getElementsByClassName("completed").length == 5) {
+                //     document.getElementById("sum").innerHTML = currentSum;
+                //     showTheSum();
+                // }
+            }
+        };
+    }(button, errHandler);
+    var span = button.getElementsByClassName("unread")[0];
+    span.innerHTML = "...";
+    span.classList.remove("hide");
+    span.classList.add("show");
+    xmlHttp.open("GET", "/", true);
+    xmlHttp.send(null);
+}
+
+function bubbleHandler(message, currentSum) {
+    var bubble = document.getElementById("sum").innerHTML = currentSum;
+    document.getElementById("lastMessage").innerHTML = "楼主异步调用战斗力感人，目测不超过" + currentSum;
+    document.getElementById("lastMessage").classList.remove("disappear");
+    showTheSum();
+}
 
 function nextButton() {
     var buttons = document.getElementsByClassName("button");
@@ -105,7 +295,10 @@ window.onload = function() {
     init();
     document.getElementById("button").addEventListener("mouseenter", function() {
         var buttons = document.getElementsByClassName("button");
-        getNumber(buttons[0]);
+        aHandler("这是个天大的秘密", 0, function(message, currentSum, button) {
+            button.getElementsByClassName("message")[0].innerHTML = message;
+            button.getElementsByClassName("unread")[0].innerHTML = "X";
+        });
     });
     document.getElementById("button").addEventListener("mouseleave", init);
 }
